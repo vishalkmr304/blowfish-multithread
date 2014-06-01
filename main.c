1cee8a483901e78d1630f120169238e53915ab47
Alessandro Renzi <alessandro.renzi@about.me>
2014-06-02 01:18:06 +0200

    Implemented context creation. Implemented reminder section.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 char mode;
 int max_threads;
+int *thread_args;
 long int input_file_length;
 long int block_size;
 
@@ -55,8 +56,12 @@ void *Blowfish_thread(void *args)
 		
 		pthread_mutex_lock(&write_mutex);
 			fseek(output_file, base+offset, SEEK_SET);
-			fwrite(&out_data, 8, block_number, output_file);
-			//TODO: ferror() to check for writing errors
+			fwrite(&out_data, 8, 1, output_file);
+			if(ferror(output_file))
+			{
+				perror("Writing error\n");
+				exit(EXIT_FAILURE);
+			}
 		pthread_mutex_unlock(&write_mutex);
 	}
 	
@@ -161,7 +166,7 @@ int main(int argc, char **argv)
 	char key[key_file_length];
 	fscanf(key_file, "%s", key);
 	
-	//TODO: Create context
+	Blowfish_Init(ctx, key, key_file_length);
 	//TODO: Test if could be usefull perform this step in a separate thread
 	
 	
@@ -176,11 +181,12 @@ int main(int argc, char **argv)
 	input_file_length = ftell(input_file);	// Get the length
 	rewind(input_file);		// Go back to the beginning
 	
-	if(input_file_length < 1)
+	if(input_file_length < 8)
 	{
-		perror("Empty input file\n");
+		perror("Input file is too short\n");
 		exit(EXIT_FAILURE);
 	}
+
 	
 	block_size = input_file_length / max_threads;
 	if(0 != (block_size%8))
@@ -188,7 +194,11 @@ int main(int argc, char **argv)
 		// Make the block size multiple of 64 bits
 		block_size -= (block_size%8);
 	}
-	long int reminder_size = block_size * max_threads;
+	long int reminder_size = (block_size * max_threads) - input_file_length;
+	long int reminder_size_alligned = reminder_size - (reminder_size%8);
+	int padding_size = 8 - (reminder_size%8);
+	
+	
 	
 	
 	///////////////////////////////////////////////////////////////////////
@@ -197,7 +207,13 @@ int main(int argc, char **argv)
 	
 	//pthread_t thread_pool[max_threads];
 	pthread_t *thread_pool = (pthread_t *) malloc(max_threads * sizeof(pthread_t));
+	thread_args = (int *) malloc(max_threads * sizeof(int));
 	
+	int i = 0;
+	for(i = 0; i < max_threads; ++i)
+	{
+		thread_args[i] = i;
+	}
 	
 	
 	////////////////
@@ -205,20 +221,18 @@ int main(int argc, char **argv)
 		clock_t start = clock();
 	////////////////
 	//TODO: Move benchmark marker
+	//TODO: implement it as a compile flag
 	
 	pthread_mutex_init(&read_mutex, NULL);
 	pthread_mutex_init(&write_mutex, NULL);
 	
 
 	
-	for(i = 0; i < block_number; i++)
+	for(i = 0; i < max_threads; i++)
 	{
 		int result;
-		
-// 		pthread_mutex_lock(&mutex);
-// 		running_thread++;
-// 		pthread_mutex_unlock(&mutex);
-// 		result = pthread_create(&thread_pool[i], NULL, DES_enc, (void *)(&args_pool[i]));
+		//TODO: Start threads
+		result = pthread_create(&thread_pool[i], NULL, Blowfish_thread, (void *)(&thread_args[i]));
 		
 		if(result != 0)
 		{
@@ -229,7 +243,77 @@ int main(int argc, char **argv)
 	
 	
 	
-	//TODO: Implement reminder
+	///////////////////////////////////////////////////////////////////////
+	// Reminder
+	///////////////////////////////////////////////////////////////////////
+	long int base_rem = block_size * max_threads;
+	uint64_t in_data_rem = 0;
+	uint64_t out_data_rem = 0;
+	
+	for(i = 0; i<reminder_size_alligned; i += 8)
+	{
+		pthread_mutex_lock(&read_mutex);
+			fseek(input_file, base_rem+i, SEEK_SET);
+			fread(&in_data_rem, 8, 1, input_file);
+		pthread_mutex_unlock(&read_mutex);
+		
+		if(mode == 'e')
+		{
+			out_data_rem = BlowfishEncryption(ctx, in_data_rem);
+		}
+		else
+		{
+			out_data_rem = BlowfishDecryption(ctx, in_data_rem);
+		}
+		
+		pthread_mutex_lock(&write_mutex);
+			fseek(output_file, base_rem+i, SEEK_SET);
+			fwrite(&out_data_rem, 8, 1, output_file);
+			if(ferror(output_file))
+			{
+				perror("Writing error\n");
+				exit(EXIT_FAILURE);
+			}
+		pthread_mutex_unlock(&write_mutex);
+	}
+	
+	
+	
+	if(mode == 'e')
+	{
+		pthread_mutex_lock(&read_mutex);
+			fseek(input_file, base_rem+i, SEEK_SET);
+			fread(&in_data_rem, reminder_size-reminder_size_alligned, 1, input_file);
+		pthread_mutex_unlock(&read_mutex);
+		
+		int j = 0;
+		for(j = 0; j < padding_size; ++j)
+		{
+			in_data_rem = (in_data_rem<<8) || (uint8_t)padding_size;
+		}
+		
+		out_data_rem = BlowfishEncryption(ctx, in_data_rem);
+		
+		pthread_mutex_lock(&write_mutex);
+			fseek(output_file, base_rem+i, SEEK_SET);
+			fwrite(&out_data_rem, 8, 1, output_file);
+			if(ferror(output_file))
+			{
+				perror("Writing error\n");
+				exit(EXIT_FAILURE);
+			}
+		pthread_mutex_unlock(&write_mutex);
+	}
+	else
+	{
+		unsigned int trim_len = out_data_rem & (uint64_t)0xFF;
+		ftruncate(output_file, input_file_length-trim_len);
+	}
+	
+	
+	
+	//TODO: join threads
+	
 	
 	////////////////
 	// Benchmark
